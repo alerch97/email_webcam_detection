02be6ac7cdecc10648fa31031b0d86657488ec0d
alerch97 <alexlerch76@gmail.com>
2024-01-21 16:18:13 +0100

    Preproccessed frame (grayscale and gaussian blur)

--- main.py
+++ main.py
@@ -7,7 +7,9 @@ time.sleep(1)
 
 while True:
     check, frame = video.read()
-    cv2.imshow("My video", frame)
+    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+    cv2.imshow("My video", gray_frame_gau)
 
     key = cv2.waitKey(1)
 
