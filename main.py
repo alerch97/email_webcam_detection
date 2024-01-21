1a9dea5c73446647154b402e2885130e519831e5
alerch97 <alexlerch76@gmail.com>
2024-01-21 16:49:11 +0100

    Add frame around new objects in the video

--- main.py
+++ main.py
@@ -1,15 +1,36 @@
 import cv2
 import time
 
-
 video = cv2.VideoCapture(0)
 time.sleep(1)
+
+first_frame = None
 
 while True:
     check, frame = video.read()
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
     cv2.imshow("My video", gray_frame_gau)
+
+    if first_frame is None:
+        first_frame = gray_frame_gau
+
+    # Process delta of frames
+    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
+    thresh_frame = cv2.threshold(delta_frame, 50, 255, cv2.THRESH_BINARY)[1]
+    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+    cv2.imshow("My video", dil_frame)
+
+    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in contours:
+        if cv2.contourArea(contour) < 5000:
+            continue
+        x, y, w, h = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+
+    cv2.imshow("Video", frame)
 
     key = cv2.waitKey(1)
 
